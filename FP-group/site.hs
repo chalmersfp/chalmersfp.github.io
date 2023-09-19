26d1e75e94daa4fa3283cef65ca6bf99c6b19edf
Patrik Jansson <patrik.ja@gmail.com>
2023-09-19 16:20:01 +0200

    Test of new site (very drafty).

--- FP-group/site.hs
+++ FP-group/site.hs
@@ -5,8 +5,13 @@ import           Hakyll
 
 
 --------------------------------------------------------------------------------
+config :: Configuration
+config = defaultConfiguration
+  { destinationDirectory = "../docs"
+  }
+
 main :: IO ()
-main = hakyll $ do
+main = hakyllWith config $ do
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
